d3db26df445a2b9aed96467061cd1a37916a0baf
Brian Mancini <bmancini@gmail.com>
2018-03-16 16:06:21 -0400

    Add periodic loading and caching of graph data

--- src/server/apis/api-network.js
+++ src/server/apis/api-network.js
@@ -1,12 +1,24 @@
+const winston = require('winston');
 const express = require('express');
+const scheduler = require('node-schedule');
 const graphService = require('../domain/graph-service');
 const app = express();
+let _graph;
 
 app.get('/api/graph', (req, res, next) => getGraph(req, res).catch(next));
 
 module.exports = app;
 
+// schedule graph loading
+scheduler.scheduleJob('0 * * * * *', loadGraph);
+function loadGraph() {
+  graphService
+    .loadGraph()
+    .then(graph => (_graph = graph))
+    .catch(winston.error);
+}
+setTimeout(loadGraph, 1000);
+
 async function getGraph(req, res) {
-  let data = await graphService.loadGraph();
-  res.send(data);
+  res.send(_graph);
 }
