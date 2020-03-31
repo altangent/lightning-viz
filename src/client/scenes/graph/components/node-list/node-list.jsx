35477f43b4e96710c6f4cc3bc1da9ffdbd241b9b
Brian Mancini <bmancini@gmail.com>
2020-03-31 08:31:38 -0400

    sort nodes in node list
    Closes #3

--- src/client/scenes/graph/components/node-list/node-list.jsx
+++ src/client/scenes/graph/components/node-list/node-list.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NodeListItem } from './node-list-item';
 
-export const NodeList = ({ nodes, ...props }) => (
-  <ul className="node-list">
-    {nodes.map(node => <NodeListItem key={node.pub_key} node={node} {...props} />)}
-  </ul>
-);
+export const NodeList = ({ nodes, ...props }) => {
+  console.log(nodes[1000]);
+  nodes = nodes.slice().sort((a, b) => (a.alias || a.pub_key).localeCompare(b.alias || b.pub_key));
+  return (
+    <ul className="node-list">
+      {nodes.map(node => (
+        <NodeListItem key={node.pub_key} node={node} {...props} />
+      ))}
+    </ul>
+  );
+};
 
 NodeList.propTypes = {
   nodes: PropTypes.array.isRequired,
